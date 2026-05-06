7f5f6ab9a6996381912f63b29ff28aa9146198fb
Isaias Piña <iax7@users.noreply.github.com>
2026-05-06 13:12:35 -0600

    fix: wpa update breaks links

--- vite.config.ts
+++ vite.config.ts
@@ -61,6 +61,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        clientsClaim: false,
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
